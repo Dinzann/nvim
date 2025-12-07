85a082369102a90210b30d9d4c11af83e2f6de31
Dinzann <yaohongyu3262@gmail.com>
2025-12-07 21:44:31 +0800

    lol

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     opts = {
       auto_install = true,
       ensure_installed = { "c", "vim", "vimdoc", "query", "elixir", "heex", "javascript", 
-			 "html", "markdown", "markdown_inline", "yaml", "latex", "typst" },
+			 "html", "markdown", "markdown_inline", "yaml",  "typst" },
       sync_install = false,
       highlight = { enable = true,uadditional_vim_regex_highlighting = false },
       indent = { enable = true },
