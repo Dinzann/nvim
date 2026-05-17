5567abfdb9267c844650cef991a796c76b8be961
Dinzann <yaohongyu3262@gmail.com>
2026-05-17 13:29:08 +0800

    fix

--- lua/custom/config/comfylnum.lua
+++ lua/custom/config/comfylnum.lua
@@ -1,22 +1,29 @@
 local configured = false
 
 local function load_comfylnum()
-	if configured then
-		return
-	end
+    if configured then
+        return
+    end
 
-	configured = true
-	vim.pack.add({
-		{ src = "https://github.com/mluders/comfy-line-numbers.nvim" },
-	})
+    configured = true
+    vim.pack.add({
+        { src = "https://github.com/mluders/comfy-line-numbers.nvim" },
+    })
 
-	require("comfy-line-numbers").setup()
+    require("comfy-line-numbers").setup()
+
+    if vim.bo.filetype == "snacks_dashboard" then
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+        vim.opt_local.statuscolumn = ""
+        vim.opt_local.signcolumn = "no"
+    end
 end
 
 vim.api.nvim_create_autocmd("UIEnter", {
-	group = vim.api.nvim_create_augroup("LoadComfyLineNumbers", { clear = true }),
-	once = true,
-	callback = function()
-		vim.schedule(load_comfylnum)
-	end,
+    group = vim.api.nvim_create_augroup("LoadComfyLineNumbers", { clear = true }),
+    once = true,
+    callback = function()
+        vim.schedule(load_comfylnum)
+    end,
 })
