8b7c52e09b2fc745a4f9f62e626599985360b656
Dinzann <yaohongyu3262@gmail.com>
2026-06-07 17:04:22 +0800

    fix

--- lua/custom/config/comment.lua
+++ lua/custom/config/comment.lua
@@ -3,44 +3,44 @@
 local lz = require("lz.n")
 
 lz.load({
-    {
-        "Comment.nvim",
-        before = function()
-            vim.pack.add({
-                { src = "https://github.com/numToStr/Comment.nvim" },
-            })
-        end,
-        keys = {
-            { "<leader>/", "gcc", mode = "n", remap = true, desc = "Toggle comment line" },
-            { "<leader>/", "gc",  mode = "v", remap = true, desc = "Toggle comment visual" },
-        },
-        after = function()
-            require("Comment").setup({
-                padding = true,
-                sticky = true,
-                ignore = "^$",
-                toggler = {
-                    line = "gcc",
-                    block = "gbc",
-                },
-                opleader = {
-                    line = "gc",
-                    block = "gb",
-                },
-                mappings = {
-                    basic = true,
-                    extra = true,
-                },
-                extra = {
-                    above = "gcO",
-                    below = "gco",
-                    eol = "gcA",
-                },
-                pre_hook = function() end,
-                post_hook = function() end,
-            })
-            vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Toggle comment line" })
-            vim.keymap.set("v", "<leader>/", "gc", { remap = true, desc = "Toggle comment visual" })
-        end,
-    },
+	{
+		"Comment.nvim",
+		before = function()
+			vim.pack.add({
+				{ src = "https://github.com/numToStr/Comment.nvim" },
+			})
+		end,
+		keys = {
+			{ "<leader>/", "gcc", mode = "n", remap = true, desc = "Toggle comment line" },
+			{ "<leader>/", "gc", mode = "v", remap = true, desc = "Toggle comment visual" },
+		},
+		after = function()
+			require("Comment").setup({
+				padding = true,
+				sticky = true,
+				ignore = "^$",
+				toggler = {
+					line = "gcc",
+					block = "gbc",
+				},
+				opleader = {
+					line = "gc",
+					block = "gb",
+				},
+				mappings = {
+					basic = true,
+					extra = true,
+				},
+				extra = {
+					above = "gcO",
+					below = "gco",
+					eol = "gcA",
+				},
+				pre_hook = function() end,
+				post_hook = function() end,
+			})
+			vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Toggle comment line" })
+			vim.keymap.set("v", "<leader>/", "gc", { remap = true, desc = "Toggle comment visual" })
+		end,
+	},
 })
